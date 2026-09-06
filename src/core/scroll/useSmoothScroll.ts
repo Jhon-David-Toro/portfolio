27cfdce9d27ccafe475ee297f3ad91b40d989809
Jhon-David-Toro <toromurieljhon@gmail.com>
2026-09-06 13:06:21 -0500

    fix: let Lenis yield to nested scrollable elements

--- src/core/scroll/useSmoothScroll.ts
+++ src/core/scroll/useSmoothScroll.ts
@@ -23,6 +23,7 @@ export function useSmoothScroll() {
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      allowNestedScroll: true,
     })
     setLenisInstance(lenis)
 
